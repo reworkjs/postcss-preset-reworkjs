ddab37796d71ffca6180b3945228fdf0371243c0
Guylian Cox <gc@pictawall.com>
2017-04-05 12:52:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# postcss-preset-reworkjs
+
+This package contains the PostCSS configuration used by default by reworkjs-based applications. You can
+read more on its usage inside the [reworkjs](https://github.com/reworkjs/reworkjs/blob/develop/docs/postcss.md) documentation.
+
+## Install
+
+`npm install --save-dev @reworkjs/postcss-preset-reworkjs`
---
+++ index.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-commonjs */
+/* eslint-disable global-require */
+
+module.exports = {
+  syntax: require('postcss-scss'),
+  plugins: [
+    // Add a :focus to every :hover
+    require('postcss-focus')(),
+
+    // Allow future CSS features to be used, also auto-prefixes the CSS...
+    require('postcss-cssnext')({ browsers: ['last 4 versions', 'IE > 10', '> 0.5%'] }),
+
+    // Posts messages from plugins to the terminal
+    require('postcss-reporter')({
+      clearMessages: true,
+    }),
+  ],
+};
